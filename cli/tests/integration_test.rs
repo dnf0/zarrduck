562ecdef1ef2a134eb1d700dbe9b479604f115a0
Daniel Fisher <daniel.fisher@climate-x.com>
2026-05-19 10:49:57 +0100

    test: add integration test for zarrduck search command

--- cli/tests/integration_test.rs
+++ cli/tests/integration_test.rs
@@ -42,3 +42,17 @@ fn test_cli_completions_bash() {
         .success()
         .stdout(predicate::str::contains("_zarrduck() {"));
 }
+
+#[test]
+fn test_cli_search_invalid_api() {
+    let mut cmd = Command::cargo_bin("zarrduck").unwrap();
+    cmd.arg("search")
+        .arg("--api")
+        .arg("http://invalid-stac-api-that-does-not-exist.com")
+        .arg("--collection")
+        .arg("era5")
+        .arg("--output=json")
+        .assert()
+        .failure()
+        .stdout(predicate::str::contains(r#""status":"error""#));
+}
